e75975a35cb46fa2975bb19b67f13a125dec95d2
Harshit Shreshthi <harshit.s@goldeneagle.ai>
2024-07-04 19:09:42 +0530

    Add AMI id in dummy.tf file

--- dummy.tf
+++ dummy.tf
@@ -18,22 +18,3 @@ variable "image_id" {
 variable "instance_type" {
   type = string
 }
-variable "ami" {
-  type = string
-  default = "ami2320320332"
-}
-variable "test123" {
-  type = string
-}
-variable "test12345" {
-  type = string
-}
-variable "testing_merge" {
-  type = string
-}
-variable "testing_merge_output" {
-  type = string
-}
-variable "testing_merge_output123" {
-  type = string
-}
