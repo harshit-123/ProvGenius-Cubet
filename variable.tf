4b27137ac257bc84f6b86c4980d1a4013e130576
Harshit Shreshthi <harshit.s@goldeneagle.ai>
2024-07-05 10:47:33 +0530

    Add varaiable.tf file

---
+++ variable.tf
@@ -0,0 +1,29 @@
+
+resource "null_resource" "dummy" {}
+variable "test" {
+    type = string
+    default = "test"
+} 
+variable "access_key"{
+    type = string
+    default = "access_key"
+}
+variable "secret_key"{
+    type = string
+    default = "secret_key"
+}
+variable "instance_type"{
+    type = string
+}
+variable "resource_type"{
+    type = string
+}
+variable "resource_type1"{
+    type = string
+}
+variable "resource_type2"{
+    type = string
+}
+variable "resource_type3"{
+    type = string
+}
